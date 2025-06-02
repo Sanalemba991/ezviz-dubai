33766783681b09a50922e6e2d802508b659c205f
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2025-06-02 14:18:07 +0530

    add whatsapp

--- src/components/Whatsapp.tsx
+++ src/components/Whatsapp.tsx
@@ -1,14 +1,16 @@
 import { FloatingWhatsApp } from 'react-floating-whatsapp'
 
 const Whatsapp = () => {
-   
-    return <FloatingWhatsApp  
-    accountName='Ezviz Dubai' 
-    phoneNumber='+971552929644' 
- 
-    statusMessage="Live chat now"
-    chatMessage="Welcome to Ezviz Dubai 🤝. How can we help?"
-    />
+    return (
+        <div className="whatsapp-container">
+            <FloatingWhatsApp  
+                accountName='Ezviz Dubai' 
+                phoneNumber='+971552929644' 
+                statusMessage="Live chat now"
+                chatMessage="Welcome to Ezviz Dubai 🤝. How can we help?"
+            />
+        </div>
+    )
 }
 
 export default Whatsapp;
